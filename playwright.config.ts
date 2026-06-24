cb4107529859f459d6ad4e688ba524a7f97c06a9
Gayatri G <gayatri.aghd@gmail.com>
2026-06-24 23:15:15 +0530

    config file update

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
   },
 
   /* Configure projects for major browsers */
