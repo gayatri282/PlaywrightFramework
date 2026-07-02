ce5009fd8a79e22604412ddcf178f8c0e240cc4a
Gayatri G <gayatri.aghd@gmail.com>
2026-07-02 17:09:07 +0530

    added test cases and jenkins file

--- src/api/ApiHelper.ts
+++ src/api/ApiHelper.ts
@@ -15,8 +15,30 @@ export class Apihelper {
             data: data,
             headers: headers
         });
-        console.log('response: ',await response.json());
+        console.log('response: ', await response.json());
         return {
+            status: response.status(),
+            body: await response.json()
+        }
+    }
+
+    async delete(endPoint: string, headers?: Record<string, string>) {
+        let response = await this.request.delete(`${this.baseURL}${endPoint}`, {
+            headers: headers
+        });
+        console.log('response status: ', response.status());
+        return {
+            status: response.status(),
+        }
+    }
+
+    async patch(endPoint: string, data: object, headers?: Record<string,string>){
+        let response = await this.request.patch(`${this.baseURL}${endPoint}`,{
+            headers: headers,
+            data: data
+        });
+        console.log('response status ', response.status());
+        return{
             status: response.status(),
             body: await response.json()
         }
--- tests/api/apitests.spec.ts
+++ tests/api/apitests.spec.ts
@@ -1,3 +1,4 @@
+import { Apihelper } from '../../src/api/ApiHelper';
 import { test, expect } from '../../src/fixtures/apiFixtures'
 
 const TOKEN = process.env.API_TOKEN!;
@@ -17,3 +18,45 @@ test('post api test', async ({ apiHelper }) => {
     let userid = response.body.id;
     console.log("user id ", userid);
 });
+
+test('delete api test', async ({ apiHelper }) => {
+    let userdata = {
+        "name": "GG API",
+        "email": `g3_api${Date.now()}@apitesting.com`,
+        "gender": "female",
+        "status": "active"
+    }
+    let response = await apiHelper.post('/public/v2/users', userdata, AUTH_HEADER);
+    expect(response.status).toBe(201);
+    expect(response.body.name).toBe(userdata.name);
+    let userid = response.body.id;
+    console.log("user id ", userid);
+
+    let delResponse = await apiHelper.delete(`/public/v2/users/${userid}`, AUTH_HEADER);
+    console.log("status: ", delResponse.status);
+    expect(response.status).toBe(204);
+})
+
+test('update api test', async ({ apiHelper }) => {
+    let userdata = {
+        'name': "SuGa",
+        'email': `api_${Date.now()}@testing.com`,
+        'gender': 'male',
+        'status': 'inactive'
+    }
+
+    let response = await apiHelper.post('/public/v2/users', userdata, AUTH_HEADER);
+    expect(response.status).toBe(201);
+    expect(response.body.name).toBe('SuGa');
+    console.log("email before update: ", response.body.email);
+    let userid = response.body.id;
+
+    let updateData= {
+        'email':`Suga_${Date.now()}@testing.com`
+    }
+
+    let updateResponse = await apiHelper.patch(`/public/v2/users/${userid}`,updateData,AUTH_HEADER);
+
+    expect(updateResponse.status).toBe(200);
+    console.log("updated email: ", updateResponse.body.email);
+})
